230f33062d3e82cd9eef0a38e5628c400007713c
Azim Muradov <azim.muradov.dev@gmail.com>
2024-11-10 14:36:38 +0300

    Store configuration parameters

--- backend/library.c
+++ backend/library.c
@@ -5,6 +5,10 @@
 #include <stdlib.h>
 
 static CatState *backend_g_state = NULL;
+static size_t backend_g_cat_count = 0;
+static double backend_g_map_width = 0.0;
+static double backend_g_map_height = 0.0;
+static double backend_g_fight_radius = 0.0;
 
 CatState *backend_init(
     const size_t cat_count,
@@ -17,9 +21,17 @@ CatState *backend_init(
     printf("map_height = %f\n", map_height);
     printf("fight_radius = %f\n", fight_radius);
 
-    backend_g_state = calloc(cat_count, sizeof(CatState));
+    void *state = calloc(cat_count, sizeof(CatState));
 
-    return backend_g_state;
+    if (state != NULL) {
+        backend_g_state = state;
+        backend_g_cat_count = cat_count;
+        backend_g_map_width = map_width;
+        backend_g_map_height = map_height;
+        backend_g_fight_radius = fight_radius;
+    }
+
+    return state;
 }
 
 void backend_update_state(void) {
@@ -29,5 +41,10 @@ void backend_update_state(void) {
 
 void backend_dispose(void) {
     free(backend_g_state);
+
     backend_g_state = NULL;
+    backend_g_cat_count = 0;
+    backend_g_map_width = 0.0;
+    backend_g_map_height = 0.0;
+    backend_g_fight_radius = 0.0;
 }
