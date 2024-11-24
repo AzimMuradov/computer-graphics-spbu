8534ea93217fa3e5187953257ba515a8d1139535
Azim Muradov <azim.muradov.dev@gmail.com>
2024-11-24 17:29:14 +0300

    Use native canvas for performance

--- ui-poc-cmp/src/main/kotlin/drunkcats/App.kt
+++ ui-poc-cmp/src/main/kotlin/drunkcats/App.kt
@@ -1,20 +1,21 @@
 package drunkcats
 
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.window.singleWindowApplication
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import org.jetbrains.skia.Color
+import org.jetbrains.skia.Paint
 import kotlin.random.Random
 import kotlin.random.nextInt
 
@@ -30,14 +31,14 @@ fun main() {
         LaunchedEffect(Unit) {
             cs.launch {
                 while (true) {
-                    catsInfo = List(50000) {
+                    catsInfo = List(50_000) {
                         CatInfo(
                             x = random.nextFloat(),
                             y = random.nextFloat(),
                             state = CatState.entries[random.nextInt(0..2)]
                         )
                     }
-                    delay(500)
+                    delay(40)
                 }
             }
         }
@@ -66,17 +67,23 @@ enum class CatState {
 
 @Composable
 fun Cats(cats: List<CatInfo>) {
-    Canvas(Modifier.fillMaxSize()) {
-        for (cat in cats) {
-            drawCircle(
-                color = when (cat.state) {
-                    CatState.Calm -> Color.Green
-                    CatState.Angry -> Color.Yellow
-                    CatState.Fighting -> Color.Red
-                },
-                radius = 8f,
-                center = Offset(size.width * cat.x, size.height * cat.y)
-            )
+    Box(
+        Modifier.fillMaxSize().drawBehind {
+            drawIntoCanvas { canvas ->
+                for (cat in cats) {
+                    canvas.nativeCanvas.drawCircle(
+                        size.width * cat.x, size.height * cat.y,
+                        radius = 8f,
+                        paint = Paint().apply {
+                            this.color = when (cat.state) {
+                                CatState.Calm -> Color.GREEN
+                                CatState.Angry -> Color.YELLOW
+                                CatState.Fighting -> Color.RED
+                            }
+                        },
+                    )
+                }
+            }
         }
-    }
+    )
 }
