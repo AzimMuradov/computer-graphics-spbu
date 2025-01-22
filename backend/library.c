37fb69e024069ba234aec0a794b93cb71c3af08f
Azim Muradov <azim.muradov.dev@gmail.com>
2025-01-22 21:03:48 +0300

    Move `convert_opengl_to_plain_coordinates` and `rand_ud` to separate files

--- backend/library.c
+++ backend/library.c
@@ -4,6 +4,9 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+#include "utils-opengl.c"
+#include "utils-random.c"
+
 #include "third-party/kdtree/kdtree.c"
 
 
@@ -14,40 +17,6 @@ static const int CAT_STATE_WANTS_TO_FIGHT = 2;
 
 static double drunk_cats_g_fight_radius = 0.0;
 static double drunk_cats_g_hiss_radius = 0.0;
-
-
-/**
- * Convert cat positions in the OpenGL coordinate system to the plain flatten coordinates.
- *
- * Every position `{x, y}` in the OpenGL coordinate system
- * will be scaled using the given window size and scale.
- *
- * The given array `[{x_1, y_1}, ..., {x_n, y_n}]`
- * will be restructured to `[x_1, y_1, ..., x_n, y_n]`.
- *
- * @param cat_count Number of cat positions given.
- * @param cat_positions Array of cat positions in the OpenGL coordinate system.
- * @param window_width Window width, must be positive.
- * @param window_height Window height, must be positive.
- * @param scale Window scale (e.g. 1.0 - no scale, 2.0 - two times scale), must be positive.
- *
- * @returns Cat positions as plain flatten coordinates.
- */
-static double *convert_opengl_to_plain_coordinates(
-    size_t cat_count,
-    const OpenGlPosition *cat_positions,
-    unsigned int window_width,
-    unsigned int window_height,
-    float scale
-);
-
-/**
- * Generate random unsigned double in the range of `[0.0, 1.0]`.
- *
- * @returns By default: random double in `[0.0, 1.0]`,
- *          or if `TEST` defined: `0.0`.
- */
-static double rand_ud(void);
 
 
 void drunk_cats_configure(const double fight_radius, const double hiss_radius) {
@@ -128,33 +97,4 @@ int *drunk_cats_calculate_states(
 
 void drunk_cats_free_states(int *states) {
     free(states);
-}
-
-
-// Internal
-
-static double *convert_opengl_to_plain_coordinates(
-    const size_t cat_count,
-    const OpenGlPosition *cat_positions,
-    const unsigned int window_width,
-    const unsigned int window_height,
-    const float scale
-) {
-    double *flat_cat_positions = malloc(cat_count * 2 * sizeof(double));
-
-    for (size_t i = 0; i < cat_count; i++) {
-        const OpenGlPosition cat_pos = cat_positions[i];
-        flat_cat_positions[2 * i] = cat_pos.x * 0.5 * window_width * scale;
-        flat_cat_positions[2 * i + 1] = cat_pos.y * 0.5 * window_height * scale;
-    }
-
-    return flat_cat_positions;
-}
-
-static double rand_ud(void) {
-#ifndef TEST
-    return (double) rand() / (double) RAND_MAX;
-#else
-    return 0.0;
-#endif
 }
